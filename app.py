f9d7be1ef7f0447b54b77c2ea72b2e9088bccea6
Cathyann22 <mahumanecathy@gmail.com>
2025-10-12 21:21:27 +0200

    Minimal test deployment

--- app.py
+++ app.py
@@ -8,4 +8,7 @@ price_range = st.slider("Set price range", 0, 100000)
 if st.button("Predict"):
     st.write(f"Running prediction with {model_choice} model for price range up to {price_range}")
 
+import shap
+import xgboost
+
 
